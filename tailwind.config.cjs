2792fdd04c73d835c768e398ddd65803055c1f54
Jean Arruda <jean.arruda@thoughtworks.com>
2022-08-06 04:05:36 -0300

    adjust config application

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,23 @@
 module.exports = {
   content: ['./index.html', './src/**/*.tsx'],
   theme: {
+    colors: {
+      'blue': '#4EA8DE',
+      'blue-dark': '#1E6F9F',
+      'danger': '#E25858',
+      'gray-100': '#F2F2F2',
+      'gray-200': '#D9D9D9',
+      'gray-300': '#808080',
+      'gray-400': '#333333',
+      'gray-500': '#262626',
+      'gray-600': '#1A1A1A',
+      'gray-700': '#0D0D0D',
+      'purple': '#8284FA',
+      'purple-dark': '#5E60CE',
+    },
+    fontFamily: {
+      sans: ['Inter', 'sans-serif'],
+    },
     extend: {},
   },
   plugins: [],
