ba12f1af56d1e185d6fead8c75c7028a50694b2b
Jean Arruda <jean.arruda@thoughtworks.com>
2022-08-06 04:14:25 -0300

    create input-text component

---
+++ src/components/input/input-text/input-text.tsx
@@ -0,0 +1,18 @@
+import { InputHTMLAttributes } from 'react'
+
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+  name: string
+}
+
+export function InputText({ name, ...props }: InputProps) {
+  return (
+    <input
+      name={name}
+      type='text'
+      className='h-[3.375rem] w-full rounded-lg border border-gray-700 bg-gray-500 p-3 
+      text-gray-100 placeholder-gray-300 transition duration-150 ease-linear hover:bg-gray-400
+      focus:border-purple focus:outline-none md:p-4'
+      {...props}
+    />
+  )
+}
