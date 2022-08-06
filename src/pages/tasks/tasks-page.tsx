affc206b5d02a11aa6771e01940082e8e7418ceb
Jean Arruda <jean.arruda@thoughtworks.com>
2022-08-06 04:15:25 -0300

    create tasks page

---
+++ src/pages/tasks/tasks-page.tsx
@@ -0,0 +1,9 @@
+import { CreateTask } from '../../components/create-task/create-task'
+
+export function TasksPage() {
+  return (
+    <main className='mx-auto mt-[-1.6875rem] flex w-full max-w-[48rem] justify-center px-4'>
+      <CreateTask />
+    </main>
+  )
+}
