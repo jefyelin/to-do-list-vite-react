c3bb488064050c6a1132008772287db94644752b
Jean Arruda <jean.arruda@thoughtworks.com>
2022-08-06 04:13:43 -0300

    create header component

---
+++ src/components/header/header.tsx
@@ -0,0 +1,11 @@
+import { Logo } from '../logo/logo'
+
+export function Header() {
+  return (
+    <header className='flex h-[11.25rem] w-full justify-center bg-gray-700 md:h-[12.5rem]'>
+      <div className='h-full w-[7.875rem] pt-[4.05rem] md:pt-[4.5rem]'>
+        <Logo />
+      </div>
+    </header>
+  )
+}
