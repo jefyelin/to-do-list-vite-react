0e2cbed19070beb59e39bb5f3f821bb38c0f2bb2
Jean Arruda <jean.arruda@thoughtworks.com>
2022-08-06 22:15:33 -0300

    adjust input-text component

--- src/components/input/input-text/input-text.tsx
+++ src/components/input/input-text/input-text.tsx
@@ -11,7 +11,7 @@ export function InputText({ name, ...props }: InputProps) {
       type='text'
       className='h-[3.375rem] w-full rounded-lg border border-gray-700 bg-gray-500 p-3 
       text-gray-100 placeholder-gray-300 transition duration-150 ease-linear hover:bg-gray-400
-      focus:border-purple focus:outline-none md:p-4'
+      focus:border-purple-dark focus:outline-none md:p-4'
       {...props}
     />
   )
