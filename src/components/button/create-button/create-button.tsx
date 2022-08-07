32ff3a63a97851e65f82090691a2c6b9c01505e8
Jean Arruda <jean.arruda@thoughtworks.com>
2022-08-06 22:12:20 -0300

    adjust create-button

--- src/components/button/create-button/create-button.tsx
+++ src/components/button/create-button/create-button.tsx
@@ -6,13 +6,13 @@ export function CreateButton({ ...props }: ButtonProps) {
   return (
     <button
       type='submit'
-      className='flex h-[3.375rem] items-center justify-center gap-2 rounded-lg bg-blue-dark p-3 
-        font-bold text-gray-100 transition duration-150 ease-linear hover:bg-blue 
-        disabled:cursor-not-allowed disabled:bg-gray-300 md:p-4'
+      className='flex h-[3.375rem] items-center justify-center gap-2 rounded-lg bg-blue-dark 
+        p-3 text-sm font-bold text-gray-100 transition duration-150 ease-linear 
+        hover:bg-blue disabled:cursor-not-allowed disabled:bg-gray-300 md:p-4'
       {...props}
     >
       Criar
-      <PlusCircle weight='bold' size={21} />
+      <PlusCircle size={21} alt='Ícone com sinal de +' />
     </button>
   )
 }
