d061f004232c2f798b6460687a0228cf0938f1ae
Jean Arruda <jean.arruda@thoughtworks.com>
2022-08-06 22:17:22 -0300

    adjust tasks page

--- src/pages/tasks/tasks-page.tsx
+++ src/pages/tasks/tasks-page.tsx
@@ -1,9 +1,33 @@
-import { CreateTask } from '../../components/create-task/create-task'
+import { useState } from 'react'
+import { CreateTaskForm } from '../../components/form/create-task-form'
+import { ConcludedTasksInfo } from '../../components/info/concluded-tasks-info'
+import { OpenTasksInfo } from '../../components/info/open-tasks-info'
+import { Task, TASK_STATUS } from '../../model/task'
 
 export function TasksPage() {
+  const [tasks] = useState<Task[]>([
+    {
+      description: 'Limpar o quarto',
+      id: '1',
+      status: TASK_STATUS.OPEN,
+    },
+    {
+      description: 'Fazer projeto',
+      id: '2',
+      status: TASK_STATUS.CONCLUDED,
+    },
+  ])
+
   return (
-    <main className='mx-auto mt-[-1.6875rem] flex w-full max-w-[48rem] justify-center px-4'>
-      <CreateTask />
+    <main
+      className='mx-auto mt-[-1.6875rem] flex w-full max-w-[48rem] flex-col justify-center 
+      px-4'
+    >
+      <CreateTaskForm />
+      <div className='mt-12 grid grid-flow-row justify-between gap-2 md:mt-16 md:grid-flow-col '>
+        <OpenTasksInfo id='open-tasks-info' tasks={tasks} />
+        <ConcludedTasksInfo id='concluded-tasks-info' tasks={tasks} />
+      </div>
     </main>
   )
 }
