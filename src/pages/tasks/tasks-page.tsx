2c4f2b0fb04f916a2f7d50b2c652beeab5601532
Jean Arruda <jean.arruda@thoughtworks.com>
2022-08-08 01:20:29 -0300

    adjust tasks page

--- src/pages/tasks/tasks-page.tsx
+++ src/pages/tasks/tasks-page.tsx
@@ -1,33 +1,90 @@
 import { useState } from 'react'
+import { EmptyTasks } from '../../components/tasks/empty'
 import { CreateTaskForm } from '../../components/tasks/form'
-import { ConcludedTasksInfo } from '../../components/tasks/info/concluded-tasks'
-import { OpenTasksInfo } from '../../components/tasks/info/open-tasks'
+import { TasksInfo } from '../../components/tasks/info/tasks-info'
+import { TasksList } from '../../components/tasks/list/container'
 import { Task, TASK_STATUS } from '../../model/task'
 
+type ChangeTaskStatus = {
+  id: string
+  newStatus: TASK_STATUS
+  tasks: Task[]
+}
+
 export function TasksPage() {
-  const [tasks] = useState<Task[]>([
-    {
-      description: 'Limpar o quarto',
-      id: '1',
-      status: TASK_STATUS.OPEN,
-    },
-    {
-      description: 'Fazer projeto',
-      id: '2',
-      status: TASK_STATUS.CONCLUDED,
-    },
-  ])
+  const [tasks, setTasks] = useState<Task[]>([])
+
+  const onCreateTask = (task: Task) => {
+    setTasks((previousTasks) => [...previousTasks, task])
+  }
+
+  const onConcludeTaskById = (id: string) => {
+    const newTasks = getTasksWithChangedTaskStatusById({
+      id,
+      newStatus: TASK_STATUS.CONCLUDED,
+      tasks,
+    })
+    setTasks(newTasks)
+  }
+
+  const onOpenTaskById = (id: string) => {
+    const newTasks = getTasksWithChangedTaskStatusById({
+      id,
+      newStatus: TASK_STATUS.OPEN,
+      tasks,
+    })
+    setTasks(newTasks)
+  }
+
+  const onRemoveTaskById = (id: string) => {
+    const newTasks = tasks.filter((task) => task.id !== id)
+
+    setTasks(newTasks)
+  }
+
+  const getTasksWithChangedTaskStatusById = ({
+    id,
+    newStatus,
+    tasks,
+  }: ChangeTaskStatus): Task[] => {
+    const newTasks = tasks.map((task) => {
+      if (task.id === id) {
+        return {
+          ...task,
+          status: newStatus,
+        }
+      }
+
+      return task
+    })
+
+    return newTasks
+  }
 
   return (
     <main
       className='mx-auto mt-[-1.6875rem] flex w-full max-w-[48rem] flex-col justify-center 
       px-4'
     >
-      <CreateTaskForm />
-      <div className='mt-12 grid grid-flow-row justify-between gap-2 md:mt-16 md:grid-flow-col '>
-        <OpenTasksInfo id='open-tasks-info' tasks={tasks} />
-        <ConcludedTasksInfo id='concluded-tasks-info' tasks={tasks} />
+      <CreateTaskForm onCreateTask={onCreateTask} />
+      <div className='mt-12 mb-6 md:mt-16'>
+        <TasksInfo tasks={tasks} />
       </div>
+      {tasks.length === 0 && (
+        <div>
+          <EmptyTasks />
+        </div>
+      )}
+      {tasks.length > 0 && (
+        <div>
+          <TasksList
+            tasks={tasks}
+            onConcludeTaskById={onConcludeTaskById}
+            onOpenTaskById={onOpenTaskById}
+            onRemoveTaskById={onRemoveTaskById}
+          />
+        </div>
+      )}
     </main>
   )
 }
