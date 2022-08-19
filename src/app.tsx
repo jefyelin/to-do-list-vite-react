f77d0a6451adb257ba73e0fab935cba0e541c887
Jean Arruda <jean.arruda@thoughtworks.com>
2022-08-18 22:58:01 -0300

    ajust vercel redirect

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import { Header } from './components/header/header'
 import { TasksPage } from './pages/tasks/tasks-page'
+import { Redirect } from './utils/router/redirect'
 
 function App() {
   return (
@@ -8,6 +9,9 @@ function App() {
       <Header />
       <Routes>
         <Route path='/' element={<TasksPage />} />
+        <Route path='/teste' element={<div>Teste</div>} />
+        <Route path='/teste2' element={<Redirect to='/teste3' />} />
+        <Route path='/teste3' element={<div>Teste3</div>} />
       </Routes>
     </Router>
   )
