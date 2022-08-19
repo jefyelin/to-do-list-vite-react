2d7999e026b1eab9dd519656f90120f41bf96a31
Jean Arruda <jean.arruda@thoughtworks.com>
2022-08-19 00:35:11 -0300

    adjust routes

--- src/app.tsx
+++ src/app.tsx
@@ -1,13 +1,15 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { HashRouter as Router, Route, Routes } from 'react-router-dom'
 import { Header } from './components/header/header'
 import { TasksPage } from './pages/tasks/tasks-page'
+import { Redirect } from './utils/router/redirect'
 
 function App() {
   return (
     <Router>
       <Header />
       <Routes>
-        <Route path='/' element={<TasksPage />} />
+        <Route path='/' element={<Redirect to='/tasks' />} />
+        <Route path='/tasks' element={<TasksPage />} />
       </Routes>
     </Router>
   )
