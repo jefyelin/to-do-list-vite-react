c378c4d86091de76cc81efb46eb2ebf38514f424
Jean Arruda <jean.arruda@thoughtworks.com>
2022-08-18 23:43:10 -0300

    netlify config

--- src/app.tsx
+++ src/app.tsx
@@ -1,15 +1,13 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import { Header } from './components/header/header'
 import { TasksPage } from './pages/tasks/tasks-page'
-import { Redirect } from './utils/router/redirect'
 
 function App() {
   return (
     <Router>
       <Header />
       <Routes>
-        <Route path='/' element={<Redirect to='/tasks' />} />
-        <Route path='/tasks' element={<TasksPage />} />
+        <Route path='/' element={<TasksPage />} />
       </Routes>
     </Router>
   )
